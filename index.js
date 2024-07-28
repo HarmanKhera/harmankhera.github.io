cebaa706e8de4ca347aecc735406db9de66cebd2
HarmandeepKhera <harmandeepsinghkhera@yahoo.com>
2024-07-28 16:22:40 +0100

    Changed REACT code

---
+++ index.js
@@ -0,0 +1,4 @@
+ReactDOM.render(<h1>Biography </h1>, document.getElementById("bio"))
+//latter is equiv. to document.querySelector('#bio')
+
+//ReactDOM.Render("<p>👋 Hi! My name is Harman. I'm...</p>", document.getElementById('bio'))
