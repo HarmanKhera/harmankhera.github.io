f104c659bc0d2cfcba4b212910ef39ab72bb3428
HarmandeepKhera <harmandeepsinghkhera@yahoo.com>
2024-07-28 16:28:58 +0100

    Fixed errors

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-ReactDOM.render(<h1>Biography </h1>, document.getElementById("bio"))
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
 //latter is equiv. to document.querySelector('#bio')
 
 //ReactDOM.Render("<p>👋 Hi! My name is Harman. I'm...</p>", document.getElementById('bio'))
